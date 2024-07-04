132ba81576cc35919987365af94d826397e0075e
1F10230194 <s1f102301943@iniad.org>
2024-07-04 14:56:05 +0900

    エラーを修正

--- todo/views.py
+++ todo/views.py
@@ -8,8 +8,7 @@ from todo.models import Task
 
 def index(request):
     if request.method == 'POST':
-        task = Task(title=request.POST['title'], 
-                    due_at=make_aware(parse_datetime(request.POST['due_at'])))
+        task = Task(title=request.POST['title'], due_at=make_aware(parse_datetime(request.POST['due_at'])))
         task.save()
     
     if request.GET.get('order') == 'due':
@@ -17,7 +16,7 @@ def index(request):
     else:
         tasks = Task.objects.order_by('-posted_at')
 
-    context={
+    context = {
         'tasks': tasks
     }
     return render(request, 'todo/index.html', context)
